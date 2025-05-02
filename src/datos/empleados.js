1e3accb7ea51d12f9f41d1d22b139675d6bfcb76
Victor Nina <viconina10@gmail.com>
2025-05-01 22:45:28 -0400

    Add files via upload

---
+++ src/datos/empleados.js
@@ -0,0 +1,50 @@
+[
+    {
+      "id": 1,
+      "nombre": "Perez Juan",
+      "cargo": "Administrador",
+      "Estado": false
+    },
+    {
+      "id": 2,
+      "nombre": "Lopez Maria",
+      "cargo": "Gerente",
+      "activo": false
+    },
+    {
+      "id": 3,
+      "nombre": "Gomez Carlos",
+      "cargo": "Desarrollador",
+      "Estado": true
+    },
+    {
+      "id": 4,
+      "nombre": "Martinez Ana",
+      "cargo": "Diseñadora",
+      "Estado": true
+    },
+    {
+      "id": 5,
+      "nombre": "Rodriguez Luis",
+      "cargo": "Soporte Técnico",
+      "Estado": false
+    },
+    {
+      "id": 6,
+      "nombre": "Fernandez Elena",
+      "cargo": "Marketing",
+      "Estado": false
+    },
+    {
+      "id": 7,
+      "nombre": "Sanchez Roberto",
+      "cargo": "Contador",
+      "Estado": false
+    },
+    {
+      "id": 8,
+      "nombre": "Diaz Gabriela",
+      "cargo": "Recursos Humanos",
+      "Estado": false
+    }
+  ]
